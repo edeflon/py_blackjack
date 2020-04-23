7878eee57b5bd6054aba04cd0e953c912210fddd
edeflon <elodie.deflon@gmail.com>
2020-04-23 10:58:00 +0200

    First commit

---
+++ blackjack.py
@@ -0,0 +1,6 @@
+from flask import *
+
+import constante.py
+
+
+
---
+++ constante.py
@@ -0,0 +1,13 @@
+CARTES = {"pique_2": 2, "pique_3": 3, "pique_4": 4, "pique_5": 5, "pique_6": 6,
+          "pique_7": 7, "pique_8": 8, "pique_9": 9, "pique_V": 10, "pique_D": 10,
+          "pique_R": 10, "pique_A": 11,
+          "trefle_2": 2, "trefle_3": 3, "trefle_4": 4, "trefle_5": 5, "trefle_6": 6,
+          "trefle_7": 7, "trefle_8": 8, "trefle_9": 9, "trefle_V": 10, "trefle_D": 10,
+          "trefle_R": 10, "trefle_A": 11,
+          "coeur_2": 2, "coeur_3": 3, "coeur_4": 4, "coeur_5": 5, "coeur_6": 6,
+          "coeur_7": 7, "coeur_8": 8, "coeur_9": 9, "coeur_V": 10, "coeur_D": 10,
+          "coeur_R": 10, "coeur_A": 11,
+          "carreau_2": 2, "carreau_3": 3, "carreau_4": 4, "carreau_5": 5, "carreau_6": 6,
+          "carreau_7": 7, "carreau_8": 8, "carreau_9": 9, "carreau_V": 10, "carreau_D": 10,
+          "carreau_R": 10, "carreau_A": 11
+          }
